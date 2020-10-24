e10ef8620a9426a05dbb7f5ea3c4e25ed85e0481
Gigabyted1 <63063779+Gigabyted1@users.noreply.github.com>
2020-10-24 06:06:00 -0400

    Items?
    Started testing with having items and furniture. Should probably take them one at a time but I'm a madman

--- 376 Project.pl
+++ 376 Project.pl
@@ -7,15 +7,15 @@ my_loc(lobby).
 room(lobby, 'The lobby of ATS. There are several chairs, a nice big doormat, and a variety of science-related decorations.').
 room(hall_down, 'The main hallway on the first floor of ATS. It is lined with doors to various classrooms.').
 room(hall_up, 'The second floor hallway is home to the computer science department of SRU, beware of the robotic creatures that dwell there.').
-room (stairway, 'The staircase is how students maneuver from the floor to floor of ATS.')
-room(auditorium, 'The aduditorium of ATS is one of the largest rooms in the building and can hold large class sizes due to its ample seating. Due to the number of students that may use the room throughout the day, objects may be left in the room by them that could assit you in the game.').
+room(stairway, 'The staircase is how students maneuver from floor to floor of ATS.').
+room(auditorium, 'The auditorium of ATS is one of the largest rooms in the building and can hold large class sizes due to its ample seating. Due to the number of students that may use the room throughout the day, objects may be left in the room by them that could assist you in the game.').
 room(bathroom_down, 'The bathroom of the first floor contains multiple sinks, soap and paper towel dispensers, and several stalls that can be used to hide in.').
 room(classroom_down, 'A fairly large room filled with several student desks and a project playing an eerie news clip about an mad SRU professor.').
 room(bathroom_up, 'The second floor bathroom contains multiple sinks, soap and paper towel dispensers, and several stalls that can be used to hide in.').
 room(classroom230, 'classroom230.').
 room(classroom232, 'classroom232.').
 room(conference_room , 'The conference room of the computer science department is where many students go to study, work on assignments or projects, and meet with friends. Objects may be found here left by students.').
-room(lair, 'The computer science department offices once held the offices of several department faculty members, now it is the lair of a mad computer scienist.').
+room(lair, 'The computer science department offices once held the offices of several department faculty members. Now it is the lair of a mad computer scientist.').
 
 /* Doors - door(<room1>, <room2>). */
 door(lobby, hall_down, '0').
@@ -29,6 +29,12 @@ door(hall_up, classroom230, '7').
 door(hall_up, classroom232, '8').
 door(hall_up, conference_room, '9').
 door(hall_up, lair, '10').
+
+/* Furniture - furn(<furn_name>, <location>, <amount>). */
+furn(dino_statue, lobby, 2).
+
+/* Items - item(<item_name>, <location>). */
+item(chewing_gum, (lobby, dinostatue, 1)).
 
 /* Doors aren't one-way */
 :- forall(door(X,Y,Z), assert(door(Y,X,Z))).
@@ -51,10 +57,14 @@ connection_exists(Here, Room) :-
 connection_exists(Here, Room) :-
   nl, write("D'oh! You can't get to that room from here."), nl, !, fail.
   
-observe :- 
+observe :-
   my_loc(Here),
   describe_room(Here), !,
   list_doors(Here), !.
+  
+look_at(Furniture, Which) :-
+  my_loc(Here),
+  forall(item(Item, (Here, Furniture, Which)), (nl, write(Item), nl)).
   
 describe_room(Here) :-
   room(Here, X, _),
