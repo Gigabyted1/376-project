06f5d2c99e74d4e2f526dc7aef14725e9ebe29d9
Gigabyted1 <63063779+Gigabyted1@users.noreply.github.com>
2020-10-26 12:45:11 -0400

    Update 376 Project.pl

--- 376 Project.pl
+++ 376 Project.pl
@@ -65,7 +65,7 @@ observe :-
   
 inspect(Furniture, Which) :-
   my_loc(Here),
-  forall(item(Item, (Here, Furniture, Which)), (nl, write(Item))).
+  forall(item(Item, Desc, (Here, Furniture, Which)), (nl, write(Item),write(' - '),write(Desc)).
   
 describe_room(Here) :-
   room(Here, Desc, _),
