fc5ce1a0b5aab671635c701e86ffbe6e73e02f3d
Gigabyted1 <63063779+Gigabyted1@users.noreply.github.com>
2020-10-27 18:47:05 -0400

    More description stuff and item stuff
    Comments, pickup command, made string names for furniture and items, added example items for game-end

--- 376 Project.pl
+++ 376 Project.pl
@@ -1,5 +1,5 @@
 /* Define our custom stuff */
-:- dynamic my_loc/1, door/2.
+:- dynamic my_loc/1, door/2, in_bag/2.
 
 my_loc(lobby).
 
@@ -31,13 +31,26 @@ door(hall_up, conference_room, '9').
 door(hall_up, lair, '10').
 
 /* Furniture - furn(<furn_name>, <location>, <amount>). */
-furn(dino_statue, lobby, 2).
+furn('Dinosaur Statue', lobby, 2).
+furn('Bookcase', classroom230, 1).
+furn('Bench', hall_up, 2).
+furn('Functional PC', classroom232, 5).
+furn('Broken PC', classroom232, 10).
 
-/* Items - item(<item_name>, <description>, <location>). */
-item(chewing_gum, 'It's chewing gum', (lobby, dino_statue, 1)).
+/* Items - item(<item_name>, <description>, (<location-room>, <location-furniture>, <which-furniture>). */
+item('Used chewing gum', 'It's chewing gum', (lobby, 'Dinosaur Statue', 1)).
+
+% These are just test items for defeating the boss, feel free to name them whatever fits with the story. I think them being Javascript related would be hilarious and perfect
+% Also, we should have each of the items be hidden behind either a puzzle or an enemy that also requires some item to defeat them.
+
+item("'JavaScript: The Definitive Guide' by David Flanagan", "A book with a somewhat disturbing aura... You feel a sense of dread just looking upon it.", (classroom230, 'Bookcase', 1)).
+item('Laptop', "A personal laptop that looks to be left in haste by a fleeing student. It still works!", (hall_up, 'Bench', 1)).
+item('USB Flash Drive', "A standard 16GB flash drive. A particular professor was crazy about these...", (classroom232, 'Functional PC', 4)).
 
 /* Doors aren't one-way */
 :- forall(door(X,Y,Z), assert(door(Y,X,Z))).
+
+% ----- Handles moving throughout the map ----- %
 
 move(Room) :-
   room_exists(Room),
@@ -57,6 +70,8 @@ connection_exists(Here, Room) :-
 connection_exists(_) :-
   nl, write("D'oh! You can't get to that room from here."), nl, !, fail.
   
+% ----- Handles describing the player's surroundings ----- %
+
 observe :-
   my_loc(Here),
   describe_room(Here), !,
@@ -78,9 +93,23 @@ list_furniture(Here) :-
 list_doors(Here) :-
   nl, write('There are doors to: '),
   forall(door(Here, ConnectedRoom, _), (nl, write(ConnectedRoom))).
+  
+% ----- Handles interacting with items ----- %
+  
+pickup(Item) :-
+  my_loc(Here), !,
+  item(Item, Desc, (Here, Furn, Which)), !,
+  retract(item(Item, Desc, (Here, Furn, Which))),
+  asserta(in_bag(Item, Desc)),
+  nl, write("You picked up "),write(Item).
+pickup(_) :-
+  nl, write("You either can't reach that item, or it doesn't exist"), !, fail.
+  
+
 
 instructions :-
   nl, write("Possible commands (use regular Prolog syntax):"),
   nl, write("start: begin the adventure!"),
   nl, write("move(<room>): move to a connected room"),
-  nl, write("observe: get information about your surroundings").
+  nl, write("observe: get information about your surroundings"),
+  nl, write("inspect: look closer at a piece of furniture").
