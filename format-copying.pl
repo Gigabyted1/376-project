e4f4561434c2926948b9f32972133dd34ca346fb
Gigabyted1 <63063779+Gigabyted1@users.noreply.github.com>
2020-11-03 17:45:11 -0500

    Update format-copying.pl

--- format-copying.pl
+++ format-copying.pl
@@ -1,2 +1,9 @@
 likes(emma, coffee).
 not(likes(emma, spider)).
+
+family(Person) :-
+  mother(Person, Mother),
+  nl, write(Mother),
+  father(Person, Father),
+  nl, write(Father),
+  forall(sibling(Person, Sibling), write(Sibling)).
